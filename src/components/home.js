2403240a141c0d41d12e5615d83349299145cb33
sui-dake <sui.vf@tuta.io>
2022-03-15 12:32:41 -0600

    SPAok

---
+++ src/components/home.js
@@ -0,0 +1,69 @@
+// eslint-disable-next-line import/no-cycle
+import { onNavigate } from '../app.js';
+
+export const Home = () => {
+  // const homeLogo = document.createElement('header');
+  const headerImg = document.createElement('img');
+  headerImg.src = 'imagenes/logo_large.png';
+  document.body.appendChild(headerImg);
+  const HomeDiv = document.createElement('div');
+  HomeDiv.setAttribute('id', 'welcomeText');
+  document.body.appendChild(HomeDiv);
+  const homeH1 = document.createElement('h1');
+  homeH1.textContent = '¡Bienvenidx a la comunidad amante de las mascotas!';
+
+  const loginInputs = document.createElement('div');
+  loginInputs.setAttribute('id', 'container1');
+  const emailInput = document.createElement('input');
+  Object.assign(emailInput, {
+    id: 'log_email',
+    class: 'mailAndPass',
+    type: 'text',
+    placeholder: 'Correo electrónico',
+  });
+  document.body.appendChild(emailInput);
+  const passInput = document.createElement('input');
+  Object.assign(passInput, {
+    id: 'log_password',
+    class: 'mailAndPass',
+    type: 'password',
+    placeholder: 'Contraseña',
+  });
+  document.body.appendChild(passInput);
+  const submitAction = document.createElement('input');
+  Object.assign(submitAction, {
+    id: 'login',
+    type: 'submit',
+    value: 'Iniciar Sesión',
+  });
+
+  document.body.appendChild(submitAction);
+  const createAcc = document.createElement('button');
+  Object.assign(createAcc, {
+    id: 'acc_creation',
+    type: 'button',
+    textContent: 'Crear cuenta',
+  });
+  document.body.appendChild(createAcc);
+  loginInputs.append(emailInput, passInput, submitAction, createAcc);
+  const googleBtnLogin = document.createElement('button');
+  googleBtnLogin.setAttribute('id', 'googleLogin', 'type', 'button', 'class', 'btn btn-secondary btn-block');
+  googleBtnLogin.textContent = 'Iniciar Sesión con Google';
+
+  createAcc.addEventListener('click', () => {
+    onNavigate('/signIn');
+  });
+
+  HomeDiv.append(
+    headerImg,
+    homeH1,
+    loginInputs,
+    googleBtnLogin,
+  );
+  return HomeDiv;
+};
+
+// revisar si está loggeada,mostrar timeline, else manda login
+// hacer de home y todas las js una function
+
+// tag for img is 'figure'
---
+++ src/components/signIn.js
@@ -0,0 +1,82 @@
+/* eslint-disable import/no-cycle */
+// import { signInFunct } from '../firebase.js';
+import { onNavigate } from '../app.js';
+
+export const SignIn = () => {
+  // const homeLogo = document.createElement('header');
+  const headerImg = document.createElement('img');
+  headerImg.src = 'imagenes/logo_small.png';
+  document.body.appendChild(headerImg);
+  const SignInDiv = document.createElement('div');
+  SignInDiv.setAttribute('id', 'welcomeText');
+  const signH1 = document.createElement('h1');
+  signH1.textContent = 'prueba 1 de xxxxxx';
+
+  const signInInputs = document.createElement('div');
+  signInInputs.setAttribute('id', 'container1');
+  const nameInput = document.createElement('input');
+  Object.assign(nameInput, {
+    id: 'log_name',
+    class: 'mailAndPass',
+    type: 'text',
+    placeholder: 'Nombre',
+  });
+  document.body.appendChild(nameInput);
+
+  const emailInput = document.createElement('input');
+  Object.assign(emailInput, {
+    id: 'log_email',
+    class: 'mailAndPass',
+    type: 'text',
+    placeholder: 'Correo Electrónico',
+  });
+  document.body.appendChild(emailInput);
+  const passInput = document.createElement('input');
+  Object.assign(passInput, {
+    id: 'log_password',
+    class: 'mailAndPass',
+    type: 'password',
+    placeholder: 'Contraseña',
+  });
+  document.body.appendChild(passInput);
+  const submitAction = document.createElement('input');
+  Object.assign(submitAction, {
+    id: 'login',
+    type: 'submit',
+    value: 'Crear cuenta',
+  });
+  document.body.appendChild(submitAction);
+  signInInputs.append(nameInput, emailInput, passInput, submitAction);
+  submitAction.addEventListener('click', () => {
+    
+    onNavigate('/');
+  });
+
+  SignInDiv.append(
+    headerImg,
+    signH1,
+    signInInputs,
+  );
+  return SignInDiv;
+};
+
+// export const signIn = `
+// <div id="task-form">
+//   <input type="text" class="mailAndPass" id = 'email' name = 'email' placeholder="E-mail">
+//   <input type="password" class="mailAndPass" id = 'password' name="password" placeholder="Password">
+//   <input type="submit" id = 'sign_up' name = 'sign_up_submit' value="Crear cuenta">
+//  </div>
+//  <script>const taskForm = document.querySelector('#task-form');
+//  console.log ('holiiiii');</script>
+// `;
+// function holi() {
+//   const taskForm = document.querySelector('#task-form');
+//   taskForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     console.log('registrado');
+//     const email = document.querySelector('#email').value;
+//     const pass = document.querySelector('#password').value;
+//     console.log(email, pass);
+//     signInFunct(email, pass);
+//   });
+// }

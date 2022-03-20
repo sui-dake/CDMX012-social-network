9586f3a8bc7962e4b88cfb57e9d4df24f2eae90b
RhoLion <rholeon@gmail.com>
2022-03-19 20:29:56 -0600

    prueba

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -1,86 +1,81 @@
 /* eslint-disable import/no-cycle */
 // import { signInFunct } from '../firebase.js';
-// eslint-disable-next-line no-unused-vars
 import { onNavigate } from '../app.js';
-import { saveForm, signInFunct } from '../firebase.js';
 
-/// ///  VISTA CREACION DE CUENTA ////////////
 export const SignIn = () => {
-  /// / LOGO E INTRODUCCION /////////////
+  // const homeLogo = document.createElement('header');
   const headerImg = document.createElement('img');
   headerImg.src = 'imagenes/logo_small.png';
-  headerImg.setAttribute('id', 'logo_hexagonal');
   document.body.appendChild(headerImg);
   const SignInDiv = document.createElement('div');
-  SignInDiv.setAttribute('id', 'welcomeText2');
+  SignInDiv.setAttribute('id', 'welcomeText');
   const signH1 = document.createElement('h1');
-  signH1.textContent = '¡Estás a punto de comenzar! Introduce tus datos';
-  /// //// FORMULARIO PARA CREAR CUENTA/////////////
+  signH1.textContent = 'prueba 1 de xxxxxx';
+
   const signInInputs = document.createElement('div');
   signInInputs.setAttribute('id', 'container1');
-
   const nameInput = document.createElement('input');
   Object.assign(nameInput, {
     id: 'log_name',
+    class: 'mailAndPass',
     type: 'text',
     placeholder: 'Nombre',
   });
-  nameInput.setAttribute('class', 'mailAndPass');
   document.body.appendChild(nameInput);
 
   const emailInput = document.createElement('input');
   Object.assign(emailInput, {
     id: 'log_email',
+    class: 'mailAndPass',
     type: 'text',
     placeholder: 'Correo Electrónico',
   });
-  emailInput.setAttribute('class', 'mailAndPass');
   document.body.appendChild(emailInput);
-
   const passInput = document.createElement('input');
   Object.assign(passInput, {
     id: 'log_password',
+    class: 'mailAndPass',
     type: 'password',
     placeholder: 'Contraseña',
   });
-  passInput.setAttribute('class', 'mailAndPass');
   document.body.appendChild(passInput);
-
-  // Boton crear cuenta
   const submitAction = document.createElement('input');
   Object.assign(submitAction, {
-    id: 'creat_acc',
+    id: 'login',
     type: 'submit',
     value: 'Crear cuenta',
   });
   document.body.appendChild(submitAction);
-
-  /// ///// PINTAR NODOS EN HTML ////////
   signInInputs.append(nameInput, emailInput, passInput, submitAction);
-
-  /// ///// INTERACCION FIRESTORE Y AUTH ////////
-  submitAction.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const name = document.querySelector('#log_name').value;
-    const email = document.querySelector('#log_email').value;
-    const password = document.querySelector('#log_password').value;
-
-    signInFunct(email, password);
-
-    saveForm(name, email, password);
+  submitAction.addEventListener('click', () => {
+    onNavigate('/');
   });
-  const footerImg = document.createElement('img');
-  footerImg.src = 'imagenes/perrito1.jpg';
-  document.body.appendChild(footerImg);
-  footerImg.setAttribute('id', 'footer_img');
+
   SignInDiv.append(
     headerImg,
     signH1,
     signInInputs,
-    footerImg,
   );
   return SignInDiv;
 };
-  // FALTA OPCION PARA RESETEAR FORMULARIO !!!! ////
-// export const signIn =
+
+// export const signIn = `
+// <div id="task-form">
+//   <input type="text" class="mailAndPass" id = 'email' name = 'email' placeholder="E-mail">
+//   <input type="password" class="mailAndPass" id = 'password' name="password" placeholder="Password">
+//   <input type="submit" id = 'sign_up' name = 'sign_up_submit' value="Crear cuenta">
+//  </div>
+//  <script>const taskForm = document.querySelector('#task-form');
+//  console.log ('holiiiii');</script>
+// `;
+// function holi() {
+//   const taskForm = document.querySelector('#task-form');
+//   taskForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     console.log('registrado');
+//     const email = document.querySelector('#email').value;
+//     const pass = document.querySelector('#password').value;
+//     console.log(email, pass);
+//     signInFunct(email, pass);
+//   });
+// }
